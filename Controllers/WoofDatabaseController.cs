5ab0d2321f4aaf1298a6515b073a1bebd0f43959
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2024-05-18 17:31:55 -0400

    Add project files.

---
+++ Controllers/WoofDatabaseController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using WebApplication1_Proof_Of_Concept.Models;
+using WebApplication1_Proof_Of_Concept.Database;
+
+namespace WebApplication1_Proof_Of_Concept.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WoofDatabaseController : ControllerBase
+    {
+
+
+        /*
+         ================= GET ROUTES =================
+         */
+
+        [HttpGet]
+        /// <summary>
+        /// Gets all users from the database.
+        /// </summary>
+        /// <returns>A list of users.</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Users>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult GetAllUsers()
+        {
+            try
+            {
+                var query = HttpContext.Request.Query;
+                string filter = query.ContainsKey("filter") ? query["filter"].ToString() : string.Empty;
+
+                List<Users> result = Connection.GetUsers();
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while retrieving users.", error = ex.Message });
+            }
+        }
+
+        public IActionResult GetBusinesses() 
+        {
+            try
+            {
+                List<Businesses> result = Connection.GetBusinesses();
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occured while retrieving businesses.", error = ex.Message });
+            }
+
+        }
+
+       /* public IActionResult GetUsersBusinesses()
+        {
+            return null;
+        }
+*/
+
+
+
+
+
+        /*
+         ================= POST ROUTES =================
+         */
+
+
+
+        /*
+         ================= PATCH ROUTES =================
+         */
+
+        /*
+         ================= DELETE ROUTES =================
+         */
+    }
+}
---
+++ Database/Connection.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Security.Principal;
+using MySql.Data.MySqlClient;
+using WebApplication1_Proof_Of_Concept.Models;
+
+namespace WebApplication1_Proof_Of_Concept.Database
+{
+    public class Connection
+    {
+
+        private static DataTable Connect(string query)
+        {
+            string connectionStr = $"server=aresfit.duckdns.org;port=5000;database=Woof_V2;uid=g_admin;pwd=dbGrbDzNNXX8gE7xL";
+
+            using (var connection = new MySqlConnection(connectionStr))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    DataTable resultTable = new DataTable();
+                    adapter.Fill(resultTable);
+                    return resultTable;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[-]ERROR "+ ex.Message);
+                    throw;
+                }
+            }
+        }
+
+        public static List<Users> GetUsers()
+        {
+            string sql = "SELECT * FROM user";
+
+            DataTable result = Connect(sql);
+            List<Users> users = new List<Users>();
+            foreach (DataRow row in result.Rows)
+            {
+                Users user = new Users();
+                user.UserID = row.Field<int>("userID");
+                user.Username = row.Field<string>("username");
+                user.Password = row.Field<string>("password");
+                user.Email = row.Field<string>("email");
+                user.ImgID = row.IsNull("imgID") ? (int?)null : row.Field<int>("imgID");
+                user.AccountType = row.Field<string>("accountType");
+                
+                users.Add(user);
+            }
+
+            return users;
+        }
+
+
+        public static List<Businesses> GetBusinesses()
+        {
+            string sql = "SELECT * FROM businesses";
+            List<Businesses> businesses = new List<Businesses>();
+            DataTable resuslt = Connect(sql);
+            foreach(DataRow row in resuslt.Rows)
+            {
+                Businesses business = new Businesses();
+                business.BusinessID = row.Field<int>("businessID");
+                business.BusinessName = row.Field<string>("businessName");
+                business.OwnerUserID = row.Field<int>("ownerUserID");
+                business.BusinessType = row.Field<string>("businessType");
+                business.Location = row.Field<string>("location");
+                business.Contact = row.Field<string>("contact");
+                business.BusinessDescription = row.Field<string>("description");
+                business.Events = row.Field<string>("events");
+                business.Rating = row.Field<string>("rating");
+                business.DataLocation = row.Field<string>("dataLocation");
+                business.ImgID = row.IsNull("imgID") ? (int?)null : row.Field<int>("imgID");
+                business.PetSizePref = row.Field<string>("petSizePref");
+                business.LeashPolicy = row.Field<bool>("leashPolicy");
+                business.DisabledFriendly = row.Field<bool>("disabledFriendly");
+                business.GeoLocation = row.Field<string>("geolocation");
+
+                businesses.Add(business);
+            }
+
+            return businesses;
+        }
+        
+        
+    }
+}
